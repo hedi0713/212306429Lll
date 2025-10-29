9b4e5a684ece1ae232ded6dc0a46a11d26e4cbd1
hedi0713 <1423697428@qq.com>
2025-10-29 17:29:45 +0800

    docs: 为 add/sub 添加模块与函数级文档字符串

--- calculator.py
+++ calculator.py
@@ -1,2 +1,11 @@
-"def add(a, b): return a + b" 
-"def sub(a, b): return a - b" 
+# 1. 修改代码
+"""
+计算器模块
+"""
+def add(a, b):
+    """返回 a + b"""
+    return a + b
+
+def sub(a, b):
+    """返回 a - b"""
+    return a - b
